2f5705457fc321c0ae51d433881f2f0b70e692fa
Anil Madhavapeddy <anil@recoil.org>
2012-11-16 12:12:30 +0000

    add authors.txt to the svn sync notification

--- vcs-sync/svn-ocaml/init.sh
+++ vcs-sync/svn-ocaml/init.sh
@@ -2,6 +2,6 @@
 # Initialise a fresh repository that fetches the INRIA SVN
 # and turns it into a git repository.
 
-git svn clone --prefix=svn/ --branches version --branches branches --tags release --trunk trunk http://caml.inria.fr/svn/ocaml
+git svn clone -A authors.txt --prefix=svn/ --branches version --branches branches --tags release --trunk trunk http://caml.inria.fr/svn/ocaml
 cd ocaml
 git remote add github git@github.com:ocaml/ocaml
