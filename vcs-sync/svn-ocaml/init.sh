962cdece90c177f7ad183ead48f325dbc9cebf0d
Jeremy Yallop <yallop@gmail.com>
2014-04-08 11:01:47 +0100

    Add an OCaml manual mirror to the git-svn scripts.

--- vcs-sync/svn-ocaml/init.sh
+++ vcs-sync/svn-ocaml/init.sh
@@ -2,6 +2,9 @@
 # Initialise a fresh repository that fetches the INRIA SVN
 # and turns it into a git repository.
 
-git svn clone -A authors.txt --preserve-empty-dirs --prefix=svn/ --branches version --branches branches --tags release --trunk trunk http://caml.inria.fr/svn/ocaml
-cd ocaml
-git remote add github git@github.com:ocaml/ocaml
+REPOS='ocaml ocamldoc'
+
+for repo in $REPOS; do
+    git svn clone -A authors.txt --preserve-empty-dirs --prefix=svn/ --branches version --branches branches --tags release --trunk trunk http://caml.inria.fr/svn/$repo
+    ( cd $repo && git remote add github git@github.com:ocaml/$repo )
+done
